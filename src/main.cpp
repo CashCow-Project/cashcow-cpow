17734d72a241fdee1c6bf800b815c253aae6d103
micro-machine <croteaumj@Michaels-MacBook-Pro.local>
2014-09-14 14:27:11 -0600

    testing 4

--- src/main.cpp
+++ src/main.cpp
@@ -2612,7 +2612,7 @@ bool LoadBlockIndex(bool fAllowNew)
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x"));
+        assert(block.hashMerkleRoot == uint256("0xbc73d188c690b218c3561f4dc45dce35d8eec943377d81ff0c1fc036bd329deb"));
         
         block.print();
         assert(block.GetHash() == (!fTestNet ? hashGenesisBlock : hashGenesisBlockTestNet));
